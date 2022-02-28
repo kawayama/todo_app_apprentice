d612f8b72ade3c6facd7b5eb693e9a1dd0d2ecac
kawayama <kawa130307@gmail.com>
2022-02-28 18:32:32 +0900

    Add category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/edit'
   root 'tasks#index'
   resources :tasks
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
