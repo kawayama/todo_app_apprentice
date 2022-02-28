21ee8ff01a41ffc1ba901d393aa869021a9e367a
kawayama <kawa130307@gmail.com>
2022-02-28 18:32:03 +0900

    Add a categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
   root 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
